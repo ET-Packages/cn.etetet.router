8763ec1508495c4f9d573587ee31f173c2b62955
tanghai <egametang@gmail.com>
2024-06-16 12:58:39 +0800

    帧同步跑通

--- Scripts/Hotfix/Server/HttpGetRouterHandler.cs
+++ Scripts/Hotfix/Server/HttpGetRouterHandler.cs
@@ -10,7 +10,8 @@ namespace ET.Server
         public async ETTask Handle(Scene scene, HttpListenerContext context)
         {
             HttpGetRouterResponse response = HttpGetRouterResponse.Create();
-            List<StartSceneConfig> realms = StartSceneConfigCategory.Instance.GetBySceneType(scene.Zone(), SceneType.Realm);
+            const int RealmZone = 2; // Realm是所有区共用的，所以独立放到单独的一个区，看StartSceneConfig配置
+            List<StartSceneConfig> realms = StartSceneConfigCategory.Instance.GetBySceneType(RealmZone, SceneType.Realm);
             foreach (StartSceneConfig startSceneConfig in realms)
             {
                 // 这里是要用InnerIP，因为云服务器上realm绑定不了OuterIP的,所以realm的内网外网的socket都是监听内网地址
