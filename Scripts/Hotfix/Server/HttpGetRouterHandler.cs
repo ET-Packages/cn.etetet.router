378836e72ee0b6c8e23bcaf7933865459d2ecbef
tanghai <egametang@gmail.com>
2024-09-04 13:16:27 +0800

    eos支持

--- Scripts/Hotfix/Server/HttpGetRouterHandler.cs
+++ Scripts/Hotfix/Server/HttpGetRouterHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Text;
 
 namespace ET.Server
 {
@@ -9,20 +10,35 @@ namespace ET.Server
     {
         public async ETTask Handle(Scene scene, HttpListenerContext context)
         {
-            HttpGetRouterResponse response = HttpGetRouterResponse.Create();
-            const int RealmZone = 2; // Realm是所有区共用的，所以独立放到单独的一个区，看StartSceneConfig配置
-            List<StartSceneConfig> realms = StartSceneConfigCategory.Instance.GetBySceneType(RealmZone, SceneType.Realm);
+            HttpGetRouterResponse httpGetRouterResponse = HttpGetRouterResponse.Create();
+            List<StartSceneConfig> realms = StartSceneConfigCategory.Instance.GetBySceneType(SceneType.Realm);
             foreach (StartSceneConfig startSceneConfig in realms)
             {
                 // 这里是要用InnerIP，因为云服务器上realm绑定不了OuterIP的,所以realm的内网外网的socket都是监听内网地址
-                response.Realms.Add(startSceneConfig.InnerIPPort.ToString());
+                httpGetRouterResponse.Realms.Add(startSceneConfig.InnerIPPort.ToString());
             }
             foreach (StartSceneConfig startSceneConfig in StartSceneConfigCategory.Instance.GetBySceneType(SceneType.Router))
             {
-                response.Routers.Add($"{startSceneConfig.StartProcessConfig.OuterIP}:{startSceneConfig.Port}");
+                httpGetRouterResponse.Routers.Add($"{startSceneConfig.StartProcessConfig.OuterIP}:{startSceneConfig.Port}");
             }
-            HttpHelper.Response(context, response);
-            await ETTask.CompletedTask;
+            
+            HttpListenerRequest request = context.Request;
+            using HttpListenerResponse response = context.Response;
+            
+            if (request.HttpMethod == "OPTIONS")
+            {
+                response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+                response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                response.AddHeader("Access-Control-Max-Age", "1728000");
+            }
+            response.AppendHeader("Access-Control-Allow-Origin", "*");
+            
+            byte[] bytes = MongoHelper.ToJson(httpGetRouterResponse).ToUtf8();
+            response.StatusCode = 200;
+            response.ContentEncoding = Encoding.UTF8;
+            response.ContentLength64 = bytes.Length;
+            await response.OutputStream.WriteAsync(bytes, 0, bytes.Length);
+            await scene.Root().GetComponent<TimerComponent>().WaitAsync(1000);
         }
     }
 }
